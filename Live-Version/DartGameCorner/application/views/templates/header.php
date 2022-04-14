1f43988b1a94626819a031bf3bdd6e46c8f8ad54
Dwi Arya Ramadhani <dwi.aryaramadhni@gmail.com>
2022-04-14 11:16:09 +0700

    Update Live-Version - #8

--- Live-Version/DartGameCorner/application/views/templates/header.php
+++ Live-Version/DartGameCorner/application/views/templates/header.php
@@ -22,7 +22,8 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?= base_url(); ?>assets/images/logo.png" type="image/x-icon">
     <title><?php echo $judul; ?> - DartGameCorner
     </title>
 </head>

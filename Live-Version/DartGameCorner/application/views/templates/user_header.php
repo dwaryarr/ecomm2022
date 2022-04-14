56fbff6d1757ce0a435d68ea3608f7fc8fe7b83b
Dwi Arya Ramadhani <dwi.aryaramadhni@gmail.com>
2022-04-14 11:15:40 +0700

    Update Live-Version - #8

--- Live-Version/DartGameCorner/application/views/templates/user_header.php
+++ Live-Version/DartGameCorner/application/views/templates/user_header.php
@@ -10,14 +10,17 @@
     <title><?php echo $judul; ?> - DartGameCorner
     </title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-    <!-- bootstrap -->
+    <!-- bootstrap 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+-->
     <link href="<?= base_url() ?>assets/css/sb_styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     <!-- SweetAlert -->
     <script src="<?= base_url() ?>assets/js/sweetalert2.all.min.js"></script>
     <!-- <script src="<?= base_url() ?>assets/js/myscript.js"></script> -->
+	    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?= base_url(); ?>assets/images/logo.png" type="image/x-icon">
 </head>
 
 <body class="sb-nav-fixed">
